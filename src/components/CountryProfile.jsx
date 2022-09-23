94c73589cebf97fc81fbdb62af49bd37b03de003
Seneca Boyer <senecaboyer90@outlook.com>
2022-09-23 03:16:55 -0400

    fixed eror

--- src/components/CountryProfile.jsx
+++ src/components/CountryProfile.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const CountryProfile = ({ country }) => {
   return (
     <div className="country-profile">
-      <div id="flag-image">
+      <h1>COUNTRY PROFILE PAGE</h1>
+      {/* <div id="flag-image">
         <img src={country.flags.png} alt={country.name.common} />
-      </div>
+      </div> */}
     </div>
   );
 };
